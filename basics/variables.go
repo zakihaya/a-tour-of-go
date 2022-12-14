e4be4299e264de6a7cdbc42903583fd94b10a59f
hayazaki <toru.hayazaki@gmail.com>
2022-12-14 09:21:58 +0900

    basics続き

---
+++ basics/variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var c, python, java bool
+
+func main() {
+	var i int
+
+	fmt.Println(i, c, python, java)
+
+	c = true
+	i = 100
+
+	fmt.Println(i, c, python, java)
+}
