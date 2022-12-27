d56c370c88a605417f29ba4faec09272ad8e4fac
hayazaki <toru.hayazaki@gmail.com>
2022-12-27 14:04:31 +0900

    Methods and interfaces 続き

---
+++ methods-and-interfaces/6-indirection.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func ScaleFunc(v *Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func main() {
+	v := Vertex{3, 4}
+	// Goは利便性のため、 v.Scale(5) のステートメントを (&v).Scale(5) として解釈します
+	v.Scale(2)
+	ScaleFunc(&v, 10)
+
+	p := &Vertex{4, 3}
+	p.Scale(3)
+	ScaleFunc(p, 8)
+
+	fmt.Println(v, p)
+}
