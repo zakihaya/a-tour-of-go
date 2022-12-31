311a9d74525e8bbbb336971ec0dc8f23a335c16d
hayazaki <toru.hayazaki@gmail.com>
2022-12-31 10:11:03 +0900

    Methods and interfaces 続き

---
+++ methods-and-interfaces/18-exercise-stringer.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type IPAddr [4]byte
+
+// Add a "String() string" method to IPAddr.
+func (addr IPAddr) String() string {
+	// strings.Join(string(addr), ":")
+
+	buf := ""
+
+	// i0 := int(addr[0])
+	// buf += strconv.Itoa(i0)
+	// buf += ":"
+	// i1 := int(addr[1])
+	// buf += strconv.Itoa(i1)
+	// buf += ":"
+	// i2 := int(addr[2])
+	// buf += strconv.Itoa(i2)
+	// buf += ":"
+	// i3 := int(addr[3])
+	// buf += strconv.Itoa(i3)
+
+	for i, v := range addr {
+		if i != 0 {
+			buf += ":"
+		}
+		buf += strconv.Itoa(int(v))
+	}
+
+	return buf
+}
+
+func main() {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
